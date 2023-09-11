ed09a5703dcb02a05eca3130fcd8266dc48be70c
Anilchauhan1995 <poojavargla1995@gmail.com>
2023-09-11 10:40:11 +0000

    WIP

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 1234567;
+let accountEmail = "admin@gmail.com";
+var accountPassword = "12345";
+accountCity = "palwal";
+let accountState;
+
+// accountId = 2;  not allowed
+
+accountEmail = "ac@gmail.com";
+accountPassword = "2243434";
+accountCity = "fbd";
+
+console.log(accountId);
