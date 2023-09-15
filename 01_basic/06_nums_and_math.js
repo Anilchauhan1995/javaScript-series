69d4da2f8095e2b7abddfdbe445f16ce29056dfc
Anilchauhan1995 <poojavargla1995@gmail.com>
2023-09-15 16:58:02 +0000

    WIP

--- 01_basic/06_nums_and_math.js
+++ 01_basic/06_nums_and_math.js
@@ -1,2 +1,36 @@
 const score = 400;
-console.log(score);
+// console.log(score);
+const Balance = new Number(100);
+
+// console.log(Balance);
+// console.log(Balance.toString());
+// console.log(Balance.toString().length);
+// console.log(Balance.toFixed(2));
+
+const otherNumber = 123.8965;
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000;
+
+// console.log(hundreds.toLocalString('en-IN')); 
+
+
+
+//+++++++++++++++++ Math ++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(4,7,3,9,5,8));
+// console.log(Math.max(4,7,3,9,5,8));
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 40;
+
+console.log(Math.floor(Math.random()*(max - min + 1))+min);
