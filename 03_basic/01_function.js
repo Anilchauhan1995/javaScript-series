209a95f296939c3959871dbca6d356656a6d190c
Anilchauhan1995 <poojavargla1995@gmail.com>
2024-04-26 12:03:40 +0000

    WIP

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -1,7 +1,35 @@
+// function addTwoNumbers(number1 , number2){
+
+//     console.log(number1 + number2)
+
+// }
+// addTwoNumbers(3,4);
+
+
 function addTwoNumbers(number1 , number2){
 
-    console.log(number1,number2)
+    //let result = number1 + number2
+
+   // return result
+
+   return number1 + number2;
 
 }
-addTwoNumbers(3,4);
+const result = addTwoNumbers(3,4);
+
+console.log(`Result:`, result);
+
+function loginUSerMessage(username = "Anil"){
+      
+    if(!username){
+
+        console.log("please Enter a UserName")
+
+        return;
+
+    }
+    return(`${username} just Logged in`)
+}
+
+console.log(loginUSerMessage());
 
