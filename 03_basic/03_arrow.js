72e5eaa236e7f79db3e048d7aed4538878543627
Anilchauhan1995 <poojavargla1995@gmail.com>
2024-04-27 17:50:37 +0000

    WIP

--- 03_basic/03_arrow.js
+++ 03_basic/03_arrow.js
@@ -10,8 +10,49 @@ const user = {
     }
     
 }
-console.log(this);
 
 // user.welcomeMessage()
 // user.username = "Chauhan"
 // user.welcomeMessage();
+// console.log(this);
+
+// function myfun(){
+
+//     let username = "anil"
+//      console.log(this.username);
+// }
+// myfun();
+
+// const myfun = function() {
+//     let username = "anil"
+//      console.log(this.username);
+
+// }
+// myfun();
+
+// const myfun = () => {
+//     let username = "anil"
+//      console.log(this);
+
+// }
+// myfun();
+
+
+// const myfun = (num1 , num2) => {
+    
+//     return num1 + num2      // expelitly arrow function add return keyword
+
+// }
+// console.log(myfun(3,4));
+
+
+// const myfun = (num1 , num2) =>  num1 + num2    // implesit return arrow function remove perenthesis and curly brakets
+
+
+// const myfun = (num1 , num2) =>  (num1 + num2)  
+// console.log(myfun(3,4));
+
+
+const myfun = (num1 , num2) =>  ({username : "Anil"})  
+console.log(myfun(3,4));
+
