a3686b647aabf0bfe0f9202bd5ae24f79ac69a49
Anilchauhan1995 <poojavargla1995@gmail.com>
2024-04-27 18:25:42 +0000

    WIP

--- 03_basic/04_iife.js
+++ 03_basic/04_iife.js
@@ -1,2 +1,20 @@
 // Immediately Invoked Function Expressions
 
+//  global scope ke polution se problum hot hai kai bar global scope ke variable ke polution ko hatane ke lye iife function ka use krta hai
+
+
+(function myfun() {
+    console.log(`DB Connected`);
+    
+}) ();
+
+
+// (  () => {                     // Arrow Function
+//     console.log(`DB Connected`);
+    
+// }) ();
+
+(  (name) => {                     // Arrow Function
+    console.log(`DB Connected with ${name}`);
+    
+}) ('Anil');
