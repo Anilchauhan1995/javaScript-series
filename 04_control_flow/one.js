08ceff29fd3acec5efaba7020223e63faa81dee1
Anilchauhan1995 <poojavargla1995@gmail.com>
2024-04-29 09:32:15 +0000

    WIP

---
+++ 04_control_flow/one.js
@@ -0,0 +1,76 @@
+// if
+
+// const isUserloggedIn = true;
+const temperature = 41;
+
+if(temperature ===41){
+    
+   console.log(`less then 50`);
+}else{
+    console.log("temperature is grater then 50");
+}
+if(3!=2){
+    
+    console.log('executed');
+}
+
+if(2=="2"){
+    
+    console.log('executed');
+}
+
+// 3!=2
+// <, >, <=, >=, ==, !=
+
+
+// const score = 200
+
+// if (score > 100) {
+//     // const power = "fly"
+//     let power = "fly"
+//     console.log(`user power : ${power}`);
+    
+// }
+// console.log(`user power : ${power}`);
+
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),
+// console.log("test2");
+
+// if (balance < 500) {
+
+//  console.log("less than");
+
+// }else if (balance < 750){
+
+//     console.log("less than 750");
+
+// }else if (balance < 900){
+
+//     console.log("less than 750");
+
+// }
+// else{
+
+//     console.log("less than 1200");
+// }
+
+
+const isUserloggedIn = true
+const debitCard = true
+
+if (isUserloggedIn && debitCard) {        //    all condition is true and code  execute
+
+    console.log("allow to buy");
+    
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {        // any one contion true and code execute
+
+    console.log("user Logged in");
+    
+}
+
