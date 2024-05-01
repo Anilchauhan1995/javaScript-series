1bcac52069bff39a50740eca99b551ddc76615b7
Anilchauhan1995 <poojavargla1995@gmail.com>
2024-05-01 12:37:29 +0000

    WIP

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -1,2 +1,47 @@
 // reduce js 
 
+const nums = [ 1,2,3 ]
+
+// const total = nums.reduce( function(acc , currval){
+
+//     console.log(`acc : ${acc} and currval : ${currval}`);
+
+//     return acc + currval
+
+// } , 0)
+
+
+// arrow function
+
+const total = nums.reduce( (acc , currval) => acc + currval , 0)
+
+console.log(total);
+
+
+const shoppingCart = [
+    {
+        itemName : "js course",
+        price : 2999
+
+    },
+    {
+        itemName : "python course",
+        price : 999
+
+    },
+    {
+        itemName : "mobile dev course",
+        price : 5999
+
+    },
+    {
+        itemName : "data science course",
+        price : 12999
+
+    }
+]
+
+const pricetoPay = shoppingCart.reduce( (acc , item) => acc + item.price , 0)
+
+console.log(pricetoPay);
+
