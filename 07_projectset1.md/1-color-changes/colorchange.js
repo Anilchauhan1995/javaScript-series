3544d3067688554cf89255f7778855d7c19314c3
Anilchauhan1995 <poojavargla1995@gmail.com>
2024-05-06 23:58:22 +0530

    WIP

--- 07_projectset1.md/1-color-changes/colorchange.js
+++ 07_projectset1.md/1-color-changes/colorchange.js
@@ -1,20 +1,57 @@
 const buttons = document.querySelectorAll('.button')
 const body = document.querySelector('body')
 // console.log(buttons);
-console.log(body);
+// console.log(body);
 
 buttons.forEach( (button)=>{
-    console.log(button)
+    // console.log(button)
     button.addEventListener('click' , function(e){
-        console.log(e);
-        console.log(e.target);
+        // console.log(e);
+        // console.log(e.target);
 
-        if (e.target === "grey") {
+        const CurrentColor = e.target.id;
+        // console.log(CurrentColor);
+        // if (e.target.id === "grey") {
 
-            body.style.backgroundColor = e.target.id
-            console.log(e.target.id);
+        //     body.style.backgroundColor = e.target.id
+        //     console.log(e.target.id);
             
+        // }
+        // if (e.target.id === "white") {
+
+        //     body.style.backgroundColor = e.target.id
+        //     console.log(e.target.id);
+            
+        // }
+        // if (e.target.id === "blue") {
+
+        //     body.style.backgroundColor = e.target.id
+        //     console.log(e.target.id);
+            
+        // }
+        // if (e.target.id === "yellow") {
+
+        //     body.style.backgroundColor = e.target.id
+        //     console.log(e.target.id);
+            
+        // }
+
+        switch(CurrentColor) {
+            case "grey" : 
+            body.style.backgroundColor = CurrentColor
+            break;
+            case "white" : 
+            body.style.backgroundColor = CurrentColor
+            break;
+            case "blue" : 
+            body.style.backgroundColor = CurrentColor
+            break;
+            case "yellow" : 
+            body.style.backgroundColor = CurrentColor
+            break;
         }
+
+
         
     })
 })
